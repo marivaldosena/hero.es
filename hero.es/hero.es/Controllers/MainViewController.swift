5fb4201e518e1aca66b36ed4c1b34e40af9ae784
Marivaldo Sena <marivaldo.sena@live.com>
2020-10-22 15:21:19 -0300

    Sketch Hero Details' screen. Fix issues related to Hero Details' screen.

--- hero.es/hero.es/Controllers/MainViewController.swift
+++ hero.es/hero.es/Controllers/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     @IBAction func openTesteController() {
         if let viewController = HeroDetailsViewController.getViewController() {
             present(viewController, animated: true, completion: nil)
