1bda661db8f2cd8121fbe250e1031b84b6ed5440
Marivaldo Sena <marivaldo.sena@live.com>
2020-11-02 19:35:33 -0300

    Refactor HeroesList storyboard, that is, remove TabBar from it. Sketch ComicsList screen.

--- hero.es/hero.es/Controllers/MainViewController.swift
+++ hero.es/hero.es/Controllers/MainViewController.swift
@@ -7,29 +7,107 @@
 
 import UIKit
 
+//MARK: - LoginType
+enum LoginServiceType {
+    case facebook
+    case google
+    case email
+}
+
+//MARK: - MainViewController: UIViewController
 class MainViewController: UIViewController {
-    @IBAction func openTesteController() {
-        var arrayTabVC: [UIViewController] = []
-        var uiNavigationController: UINavigationController
+    @IBOutlet weak var textFieldEmail: UITextField?
+    @IBOutlet weak var textFieldPassword: UITextField?
+    @IBOutlet weak var buttonFacebook: UIButton?
+    @IBOutlet weak var buttonGoogle: UIButton?
+    @IBOutlet weak var buttonLoginWithEmail: UIButton?
+    @IBOutlet weak var buttonCreateAccount: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        if let heroesListVC = UIStoryboard(name: "HeroesList", bundle: nil).instantiateInitialViewController() as? HeroesListViewController {
-            uiNavigationController = UINavigationController(rootViewController: heroesListVC)
-            heroesListVC.tabBarItem = UITabBarItem(title: "Heroes", image: UIImage(systemName: "person.3"), tag: 0)
-            
-            arrayTabVC.append(uiNavigationController)
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        switch sender {
+        case buttonLoginWithEmail:
+            self.login(with: .email)
+        case buttonFacebook:
+            self.login(with: .facebook)
+        case buttonGoogle:
+            self.login(with: .google)
+        default:
+            break
         }
+    }
+    
+    @IBAction func register(_ sender: UIButton) {
+        
+    }
+    
+    private func createTabBarItem(storyBoardName: String, tabName: String, iconName: String, tagNumber: Int) -> UIViewController? {
+        if let viewController = UIStoryboard(name: storyBoardName, bundle: nil).instantiateInitialViewController() {
+
+            viewController.tabBarItem = UITabBarItem(title: tabName, image: UIImage(systemName: iconName), tag: tagNumber)
+            
+            
+            return viewController
+        }
+        
+        return nil
+    }
+    
+    private func login(with service: LoginServiceType) {
+        switch service {
+        case .email:
+            self.loginWithEmail()
+        case .facebook:
+            self.loginWithFacebookAccount()
+        case .google:
+            self.loginWithGoogleAccount()
+        default:
+            break
+        }
+    }
+    
+    private func loginWithEmail() {
+        self.doSuccessfullCredentialsLoginBehavior()
+    }
+    
+    private func loginWithGoogleAccount() {
+        
+    }
+    
+    private func loginWithFacebookAccount() {
+        
+    }
+    
+    private func createAccount() {
+        
+    }
+    
+    private func doSuccessfullCredentialsLoginBehavior() {
+        var arrayTabVC: [UIViewController] = []
+        
+        if let heroesListVC = self.createTabBarItem(storyBoardName: "HeroesList", tabName: "Heroes", iconName: "person.3", tagNumber: 0) {
+            let navController = UINavigationController(rootViewController: heroesListVC)
+            heroesListVC.title = "Heroes"
+            arrayTabVC.append(navController)
+        }
+        
+        if let comicsListVC = self.createTabBarItem(storyBoardName: "ComicsList", tabName: "Comics", iconName: "book", tagNumber: 1) {
+            let navController = UINavigationController(rootViewController: comicsListVC)
+            comicsListVC.title = "Comics"
+            arrayTabVC.append(navController)
+        }
+        
         
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = arrayTabVC
             
         
         navigationController?.pushViewController(tabBarController, animated: true)
-        
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    
+    
 }
-
