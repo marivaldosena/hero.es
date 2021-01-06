6c2013a46b6557dc80d34a859a2af9693e186b10
Marivaldo Sena <marivaldo.sena@live.com>
2021-01-05 21:31:17 -0300

    #feature/favorites: Create FavoriteModel for storing data related to favorite data.

---
+++ hero.es/hero_es/Favorites/Views/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  hero.es
+//
+//  Created by Marivaldo Sena on 02/11/20.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
